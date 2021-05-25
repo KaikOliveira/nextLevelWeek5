ffdd5d25822df052bdfdc6d26d3f632804d047bf
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-05-25 20:33:05 -0300

    Dimensions ReactNative.getWidth

--- plantmaneger/src/pages/Welcome.tsx
+++ plantmaneger/src/pages/Welcome.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Image, Text, TouchableOpacity, View, StyleSheet, SafeAreaView } from 'react-native';
+import { 
+  Image, 
+  Text, 
+  TouchableOpacity, 
+  StyleSheet, 
+  SafeAreaView,
+  Dimensions
+} from 'react-native';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
@@ -13,7 +20,11 @@ export function Welcome() {
         de forma fácil
       </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+      <Image 
+        source={wateringImg} 
+        style={styles.image} 
+        resizeMode="contain"
+      />
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas.
@@ -33,7 +44,7 @@ const styles = StyleSheet.create({
   container:{
     flex: 1,
     alignItems:'center',
-    justifyContent:'space-between'
+    justifyContent:'space-around'
   },
   title:{
     fontSize: 32,
@@ -48,6 +59,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     color: colors.heading
   },
+  image: {
+    height: Dimensions.get('window').width * 0.7
+  },
   button:{
     backgroundColor: colors.green,
     justifyContent: 'center',
@@ -55,10 +69,10 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
-    width: 56
+    width: 56,
   },
-  image: {
-    width: 292,
-    height: 284
+  buttonText: {
+    color: colors.white,
+    fontSize: 24
   }
 })
